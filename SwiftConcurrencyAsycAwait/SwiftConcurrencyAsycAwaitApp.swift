305e40906e20e5c86e5345c88f1c22e60020324f
Viswanath <viswanath3344@gmail.com>
2023-10-03 19:23:33 +0530

    Initial Commit

---
+++ SwiftConcurrencyAsycAwait/SwiftConcurrencyAsycAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyAsycAwaitApp.swift
+//  SwiftConcurrencyAsycAwait
+//
+//  Created by Ponthota, Viswanath Reddy (Cognizant) on 03/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyAsycAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
